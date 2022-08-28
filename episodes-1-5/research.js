1e6379942db9fcac722c5cf473770ef72a9d58cb
Mr-Woo-L <93970903+Mr-Woo-L@users.noreply.github.com>
2022-08-28 18:00:00 +0200

    Add files via upload

---
+++ episodes-1-5/research.js
@@ -0,0 +1,57 @@
+document.write(`
+
+
+<hr>
+<br>
+<h2>Research Notes:</h2>
+<br>
+Html 5 removed Html styling.
+<br>
+<a href="https://www.w3schools.com/tags/tag_font.asp" target="blank" style="color:orange">https://www.w3schools.com/tags/tag_font.asp</a> 
+<br>
+<br>
+
+document.write for info that is shared between all pages:<br>
+<a href="https://www.studytonight.com/javascript/output-in-javascript" target="blank" style="color:orange" >https://www.studytonight.com/javascript/output-in-javascript</a>
+<br>
+<br>
+<a href="https://www.youtube.com/watch?v=mbkKbhSbWKA" target="blank" style="color:orange" > Writing HTML in Javascript | Javascript | Tutorial 5 </a>
+<br>
+
+<a href="https://www.youtube.com/watch?v=lyVwVgiRNIU&list=PLLAZ4kZ9dFpMSXUYwxDFOvyxlssug29Fu&index=13" target="blank" style="color:orange" > Divs & Spans | HTML | Tutorial 13</a>
+<br>
+<br>
+https://www.w3docs.com/learn-html/html-div-tag.html
+<br>
+<a href="https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/HTML_basics" target="blank" style="color:orange">https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/HTML_basics</a><br>
+<br>
+<br>
+Programmer resources:<br>
+<a href="https://x-team.com/blog/github-repos-for-programmers/" target="blank" style="color:orange">https://x-team.com/blog/github-repos-for-programmers/</a><br />
+<br>
+<br>
+<a href="https://codepen.io/2kool2/pen/WNvoJvY?editors=0010" target="blank" style="color:orange">https://codepen.io/2kool2/pen/WNvoJvY?editors=0010 </a><br>
+
+<br>
+<br>
+<br>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+`) ;
+
+
